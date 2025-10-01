2fbc0d9272559307284ac7506e732763784756f8
Abhay Ramkrushna Patil <patilabhay484@gmail.com>
2025-10-02 00:25:15 +0530

    modify todo

--- app/api/todos/[id]/route.ts
+++ app/api/todos/[id]/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { auth } from "@clerk/nextjs/server"
 import prisma from "@/lib/prisma";
 
-export async function DELETE( req : NextRequest , { params } : { params : {id : string}}){
+export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
 
     const { userId } = await auth()
 
@@ -13,5 +13,51 @@ export async function DELETE( req : NextRequest , { params } : { params : {id : 
             status: 401
         })
     }
-    
+
+    try {
+        const todoId = params.id
+        const todo = await prisma.todo.findUnique({
+            where: {
+                id: todoId
+            }
+        })
+
+        if (!todo) {
+            return NextResponse.json({
+                error: "todo not found"
+            }, {
+                status: 401
+            })
+        }
+
+        if( todo.userId !== userId ){
+            return NextResponse.json({
+                error: "todo not found"
+            }, {
+                status: 401
+            })
+        }
+
+        await prisma.todo.delete({
+            where : {
+                id : todoId
+            }
+        })
+
+        return NextResponse.json({
+            message : "todo deleted "
+            }, {
+                status: 200
+            })
+    }
+    catch (error) {
+        console.log("Error in updating subscription", error)
+        return NextResponse.json({
+            error: "Internale Server Error "
+        },
+            {
+                status: 500
+            })
+
+    }
 }
